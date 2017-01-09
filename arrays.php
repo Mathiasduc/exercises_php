6d1298f10dd5a923e6077fd442785c12e1ada34c
Mathias Ducancel <mathiasduc@gmail.com>
2017-01-09 11:43:59 +0100

    Add exo8

--- arrays.php
+++ arrays.php
@@ -1,14 +1,16 @@
 <div>
 	<?php 
-	/*$months = ["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre"];
-	$months[7]= "août" ;*/
-	$dept = ["Aisne"=> "02",
+	$months = ["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre"];
+	$months[7]= "août" ;
+	/*$dept = ["Aisne"=> "02",
 	59 => "Nord",
 	"Oise" => "60",
 	"Pas-de-Calais" => "62",
 	"Somme" => "80",
 	51 => "Reims"];
-	var_dump($dept[51]);	
-
+	var_dump($dept[51]);	*/
+	foreach ($months as $value) {
+		echo $value . "<br>";	
+	}
 	?>
 </div>
