e0c0ab3ee153d2c121ad84f75b821f1c0f5a4b45
Mathias Ducancel <mathiasduc@gmail.com>
2017-01-09 11:24:29 +0100

    Add exo 3

--- arrays.php
+++ arrays.php
@@ -1,7 +1,7 @@
 <div>
 	<?php 
 	$months = ["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre"];
-		var_dump($months[2]);
+		var_dump($months[5]);	
 
 	?>
 </div>
