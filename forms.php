f3cbd605b2837065789d980fae2835fb519b40a1
Mathias Ducancel <mathiasduc@gmail.com>
2017-01-09 14:12:13 +0100

    Add exo 5

--- forms.php
+++ forms.php
@@ -1,4 +1,9 @@
-<form action="user.php" method="post">
+<form action="index.php" method="post">
+	<select name="gender" id="gender">
+		<option value="">Civilitée</option>
+		<option value="Mr">Mr</option>
+		<option value="Mme">Mme</option>
+	</select>
 	<label for="first_name">Prenom</label>
 	<input name="first_name" type="text">
 	<label for="name">Nom</label>
