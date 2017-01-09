92d575690558a90e40ac89bf98d1624699ea0c22
Mathias Ducancel <mathiasduc@gmail.com>
2017-01-09 11:32:17 +0100

    Add exo 5

--- arrays.php
+++ arrays.php
@@ -1,8 +1,13 @@
 <div>
 	<?php 
-	$months = ["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre"];
-	$months[7]= "août" ;
-		var_dump($months);	
+	/*$months = ["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre"];
+	$months[7]= "août" ;*/
+	$dept = ["Aisne"=> "02",
+	"Nord" => "59",
+	"Oise" => "60",
+	"Pas-de-Calais" => "62",
+	"Somme" => "80"];
+	var_dump($dept);	
 
 	?>
 </div>
