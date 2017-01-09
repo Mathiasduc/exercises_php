fa5bab4f8d4e6915aef4baf79c8928ab14d2258f
Mathias Ducancel <mathiasduc@gmail.com>
2017-01-09 11:55:46 +0100

    Add exo 9

--- arrays.php
+++ arrays.php
@@ -1,16 +1,18 @@
 <div>
 	<?php 
-	$months = ["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre"];
-	$months[7]= "août" ;
-	/*$dept = ["Aisne"=> "02",
+	/*$months = ["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre"];
+	$months[7]= "août" ;*/
+	$dept = [
+	02 => "Aisne",
 	59 => "Nord",
-	"Oise" => "60",
-	"Pas-de-Calais" => "62",
-	"Somme" => "80",
-	51 => "Reims"];
-	var_dump($dept[51]);	*/
-	foreach ($months as $value) {
-		echo $value . "<br>";	
+	60 => "Oise",
+	62 => "Pas-de-Calais",
+	80 => "Somme" ,
+	51 => "Reims"
+	];	
+	foreach ($dept as $key => $value) {
+		echo "$value <br>";
 	}
+
 	?>
 </div>
