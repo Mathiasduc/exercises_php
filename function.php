178ee4c6c0a19acaee53269ec1e726d335f133e8
Mathias Ducancel <mathiasduc@gmail.com>
2017-01-09 10:48:58 +0100

    Add exo 4

--- function.php
+++ function.php
@@ -1,8 +1,14 @@
 <div>
 	<?php 
-		function yo($string1, $string2){
-			return $string1 . $string2;
+		function numCompare($num1, $num2){
+			if($num1 >$num2){
+				return "Le premier nombre est plus grand ";
+			}elseif ($num2 > $num1) {
+				return "Le premier nombre est plus petit";
+			}else{
+				return "Les deux nombres sont identiques";
+			}
 		}
-		yo("y","o");
+		var_dump(numCompare(10,10));
 	 ?>	
 </div>
