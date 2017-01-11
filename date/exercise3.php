a9e9d65e355a58b9c8b6deb75ffad913ccb9f7fd
Mathias Ducancel <mathiasduc@gmail.com>
2017-01-11 14:20:33 +0100

    Add exo3

---
+++ date/exercise3.php
@@ -0,0 +1,4 @@
+<?php 
+setlocale(LC_ALL, 'fr_FR.utf8');
+?>
+<h3><?=	strftime("%A %d %B %Y");?></h3>
--- date/index.php
+++ date/index.php
@@ -9,7 +9,7 @@
 	<div class="center frame">
 		<h1>Maitriser les dates:</h1>
 		<?php 
-		include 'exercise2.php';
+		include 'exercise3.php';
 				 ?>
 		<!-- <a href="index.php?page=otherPage">Autre page.</a>
 		<a href="index.php?page=exercise3FormLogin">Login</a>
